aa885ddd57281820cfb14f09d79e215f889464d1
MateusAparecido <72242588+MateusAparecido@users.noreply.github.com>
2022-03-17 01:52:16 -0300

    conjunto independente

--- src/Grafo.py
+++ src/Grafo.py
@@ -414,3 +414,71 @@ class Grafo:
                 if ct==0:                
                     arqOut.write(f'\nO {verticev} nao é Articulaçao!!\n')
                     ct+=1
+        #TP2
+        
+        # Python Program to implement
+        # the above approach
+        
+        # Recursive Function to find the
+        # Maximal Independent Vertex Set    
+    def graphSets(self,graph):
+            
+            # Base Case - Given Graph 
+            # has no nodes
+        if(len(graph) == 0):
+            return []
+            
+            # Base Case - Given Graph
+            # has 1 node
+        if(len(graph) == 1):
+            return [list(graph.keys())[0]]
+            
+            # Select a vertex from the graph
+        vCurrent = list(graph.keys())[0]
+            
+            # Case 1 - Proceed removing
+            # the selected vertex
+            # from the Maximal Set
+        graph2 = dict(graph)
+            
+            # Delete current vertex 
+            # from the Graph
+        del graph2[vCurrent]
+            
+            # Recursive call - Gets 
+            # Maximal Set,
+            # assuming current Vertex 
+            # not selected
+        res1 = self.graphSets(graph2)
+            
+            # Case 2 - Proceed considering
+            # the selected vertex as part
+            # of the Maximal Set
+        
+            # Loop through its neighbours
+        for v in graph[vCurrent]:
+                
+                # Delete neighbor from 
+                # the current subgraph
+            if(v in graph2):
+                del graph2[v]
+            
+            # This result set contains VFirst,
+            # and the result of recursive
+            # call assuming neighbors of vFirst
+            # are not selected
+        res2 = [vCurrent] + self.graphSets(graph2)
+            
+            # Our final result is the one 
+            # which is bigger, return it
+        if(len(res1) > len(res2)):
+            return res1
+        return res2
+        
+    def printSets(self,arqOut):
+        maximalIndependentSet = self.graphSets(self.grafo2)
+        print(maximalIndependentSet)
+        # Prints the Result 
+        arqOut.write(f'\nConjunto independente: ')
+        for i in maximalIndependentSet:
+            arqOut.write(f'{i+1} ')
